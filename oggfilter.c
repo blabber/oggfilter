6a6da2d41917095f014c09bc173e06d1bfd6d48b
Tobias Rehbein <tobias.rehbein@web.de>
2009-06-08 19:30:26 +0200

    Clean up oggfilter.c
    Third and last  commit in a row of commits refactoring the code.
    
     * rename filename[,_buffer] to path[,_buffer]
     * add a comment explaining strncpy(3) usage
     * remove TRY_MALLOC() macro

--- oggfilter.c
+++ oggfilter.c
@@ -19,8 +19,6 @@
 #include "options.h"
 
 #define MAXLINE         1024
-#define TRY_MALLOC(v,s) if ((v = malloc(s)) == NULL) err(1 ,"%s" ,#v)
-
 
 /* prototypes */
 int             main(int argc, char **argv);
@@ -28,37 +26,47 @@ int             main(int argc, char **argv);
 int
 main(int argc, char *argv[])
 {
-        char           *filename_buffer = NULL;
-        char           *filename = NULL;
+        char           *path_buffer = NULL;
+        char           *path = NULL;
         char           *in = NULL;
         char           *newline;
         struct options  opts;
-        struct conditions cond;
         struct context *ctx;
-
+        struct conditions cond;
 
         if (!setlocale(LC_ALL, ""))
                 warnx("could not set locale");
 
         parse_options(&opts, argc, argv);
 
+        /*
+         * The strncpy(3) calls in this conditional construct copy one byte
+         * more than needed to copy the string. This makes sure the string
+         * will be null terminated.
+         * 
+         * I tried to write this in a more compact way, but the resulting code
+         * was not very readable, so I'll keep it this way.
+         */
         if (opts.pathprefix != NULL) {
                 if (opts.pathprefix[strlen(opts.pathprefix) - 1] == '/') {
-                        TRY_MALLOC(filename_buffer, strlen(opts.pathprefix) * sizeof(char) + MAXLINE + 1);
-                        strncpy(filename_buffer, opts.pathprefix, strlen(opts.pathprefix) + 1);
-                        in = &filename_buffer[strlen(opts.pathprefix)];
+                        if ((path_buffer = malloc(strlen(opts.pathprefix) + MAXLINE)) == NULL)
+                                err(EX_SOFTWARE, "could not allocate memory: path_buffer");
+                        strncpy(path_buffer, opts.pathprefix, strlen(opts.pathprefix) + 1);
+                        in = &path_buffer[strlen(opts.pathprefix)];
                 } else {
-                        TRY_MALLOC(filename_buffer,
-                                   strlen(opts.pathprefix) * sizeof(char) + 1 + MAXLINE + 1);
-                        strncpy(filename_buffer, opts.pathprefix, strlen(opts.pathprefix));
-                        strncpy(&filename_buffer[strlen(opts.pathprefix)], "/", 2);
-                        in = &filename_buffer[strlen(opts.pathprefix) + 1];
+                        if ((path_buffer = malloc(strlen(opts.pathprefix) + 1 + MAXLINE)) == NULL)
+                                err(EX_SOFTWARE, "could not allocate memory: path_buffer");
+                        strcpy(path_buffer, opts.pathprefix);
+                        strncpy(&path_buffer[strlen(opts.pathprefix)], "/", 2);
+                        in = &path_buffer[strlen(opts.pathprefix) + 1];
                 }
         } else {
-                TRY_MALLOC(filename_buffer, MAXLINE + 1);
-                in = filename_buffer;
+                if ((path_buffer = malloc(MAXLINE + 1)) == NULL)
+                        err(EX_SOFTWARE, "could not allocate memory: path_buffer");
+                in = path_buffer;
         }
 
+        /* set up context */
         init_conditions(&cond);
         cond.min_length = opts.min_length;
         cond.max_length = opts.max_length;
@@ -75,17 +83,17 @@ main(int argc, char *argv[])
                         newline[0] = '\0';
 
                 if (in[0] == '/')
-                        filename = in;
+                        path = in;
                 else
-                        filename = filename_buffer;
+                        path = path_buffer;
 
-                if (check_file(filename, ctx) ^ opts.invert)
-                        printf("%s\n", filename);
+                if (check_file(path, ctx) ^ opts.invert)
+                        printf("%s\n", path);
         }
 
         /* free all resources */
-        if (filename != NULL)
-                free(filename);
+        if (path != NULL)
+                free(path);
         if (ctx != NULL)
                 context_close(ctx);
 
